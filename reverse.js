dd0dcf0e3b2cbc8c609ab0f681acd9312ad01df4
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-17 16:33:30 -0700

    First commit

---
+++ reverse.js
@@ -0,0 +1,12 @@
+/* 
+Calling the function in node with arguments inputs those arguments into the function.
+
+This will probably require using command line arguments
+
+Will need to save the arguments put in to a variable, then implement the string reverse function on each item in that variable/array.
+*/
+
+const arg = process.argv;
+
+console.log(arg);
+
