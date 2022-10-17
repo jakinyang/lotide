aca222a2e5a3292b3cd65aec7fb559c35c651397
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-17 14:40:56 -0700

    Removes test console.log cases

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = (actual, expected) => {
-  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`)
-}
+  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+};
 
 assertEqual("a", "b");
 assertEqual(1, 1);
