7b0d6857caab32397b3f12fad389e4578c7a5cd0
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-17 16:57:52 -0700

    Adds reverse function
    Reverse function takes a string and assigns the letters in reverse order to an output 'revesred' string.

--- reverse.js
+++ reverse.js
@@ -5,8 +5,25 @@ This will probably require using command line arguments
 
 Will need to save the arguments put in to a variable, then implement the string reverse function on each item in that variable/array.
 */
+// assertEqual for testing purposes
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+};
 
+
+// Extracts the relevant arguments into an array
 const arg = process.argv.slice(2);
 
+// Test arg array holds correct variables
 console.log(arg);
 
+// String reversing function
+const reverse = (string) => {
+  let reverseWord = '';
+  for (let i = string.length -1; i >= 0; i--) {
+    reverseWord += `${string[i]}`;
+  }
+  return reverseWord;
+}
+reverse('abc');
+console.log(reverse('abc'));
