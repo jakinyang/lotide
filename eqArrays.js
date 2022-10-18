0c6780c06c24d6dba9ae4eb68b6d0663432b8732
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-18 16:51:24 -0700

    First commit

---
+++ eqArrays.js
@@ -0,0 +1,3 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+};
