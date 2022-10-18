c576617e6ff63ba96712475bf9532fd09eba19f4
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-18 16:57:07 -0700

    Adds function body to eqArrays function
    Function body with check for equal lengths, and a single loop to iterate over both arrays and perform strict comparison for each value at indentical indeces.

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,17 @@
 const assertEqual = (actual, expected) => {
   actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const eqArrays = (arr1, arr2) => {
+  // Check if lengths are the same?
+  if (arr1.length !== arr2.length) return "Array lengths unequal"
+  // Single loop to loop over both arrays - since both are the same length
+  for (let i = 0; i < arr1.length; i++) {
+    // Compare each value
+    if (arr1[i] !== arr2[i]) return false; 
+  }
+  // If all comparisons pass without throwing false, return true
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
