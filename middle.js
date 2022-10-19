00cb0adfa57bc77a5efe23fc2b8afdd0c64862cf
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 14:18:57 -0700

    Adds isEven and basic middle function
    Body of middle() that checks whether the argument length is longer than two.
    
    To determine whether the middle function should return one or two values, we need to be able to determine if it is odd (single value) or even (two values).

---
+++ middle.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  // Check if lengths are the same?
+  if (arr1.length !== arr2.length) return false;
+  // Single loop to loop over both arrays - since both are the same length
+  for (let i = 0; i < arr1.length; i++) {
+    // Compare each value
+    if (arr1[i] !== arr2[i]) return false; 
+  }
+  // If all comparisons pass without throwing false, return true
+  return true;
+}
+
+const assertArraysEqual = (arr1, arr2) => {
+  eqArrays(arr1, arr2) ? console.log(`🌲🌲🌲 Assertion Passed: ${arr1} === ${arr2}`) : console.log(`🍄🍄🍄 Assertion Failed: ${arr1} !== ${arr2}`);
+}
+
+const isEven = function(num) {
+  return (num % 2 === 0);
+}
+
+const middle = function(array) {
+  let returnArray = [];
+  if (array.length < 3) return returnArray;
+  
+}
