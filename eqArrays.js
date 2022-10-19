4befd2782b3d5dbbd86450e66faa6062c9cdbb3b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-18 18:21:47 -0700

    Changes formatting

--- eqArrays.js
+++ eqArrays.js
@@ -15,3 +15,7 @@ const eqArrays = (arr1, arr2) => {
 }
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
