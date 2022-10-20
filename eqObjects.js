d373be95598b422fa413a34badc99358e8aed202
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 20:26:35 -0700

    Adds eqArray functionality
    eqArray functionality is incorporated in a conditional that tests whether the key value is an array. If it is an array, then eqArray is called to test that the arrays at that key in both objects are equal.

--- eqObjects.js
+++ eqObjects.js
@@ -11,10 +11,26 @@ const assertEqual = (actual, expected) => {
   actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
 };
 
+const eqArrays = (arr1, arr2) => {
+  // Check if lengths are the same?
+  if (arr1.length !== arr2.length) return "Array lengths unequal"
+  // Single loop to loop over both arrays - since both are the same length
+  for (let i = 0; i < arr1.length; i++) {
+    // Compare each value
+    if (arr1[i] !== arr2[i]) return false; 
+  }
+  // If all comparisons pass without throwing false, return true
+  return true;
+}
+
 const eqObjects = function (object1, object2) {
   if(Object.keys(object1).length !== Object.keys(object2).length) return false;
   for (let key in object1) {
-    if (!object2[key] || object1[key] !== object2[key]) return false;
+    if (Array.isArray(object1[key])) {
+      if (!object2[key] || eqArrays(object1[key], object2[key]) !== true) return false;
+   } else {
+      if (!object2[key] || object1[key] !== object2[key]) return false;
+   }
   }
   return true;
 }
