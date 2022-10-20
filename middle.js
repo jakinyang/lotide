904bf1bbd3a726412bb20e4f9c55f82aca123db3
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 18:27:12 -0700

    Formats file

--- middle.js
+++ middle.js
@@ -36,7 +36,7 @@ const middle = function(array) {
   return returnArray;
 }
 
-assertArraysEqual(middle([1]), []);
+/* assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); */
