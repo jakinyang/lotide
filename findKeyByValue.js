1519aba2eb8b6f88b49442ecf52a9c50a0719988
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 19:16:27 -0700

    Adds function body and tests
    findKeyByValue function body loops over the keys in the object. Key value matches the target value, the key is returned.

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+/* 
+Pseudocode
+
+Finding key by value
+*/
+
+const assertEqual = (actual, expected) => {
+  actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function (object, target) {
+  for (let key in object) {
+    console.log(key);
+    if (object[key] === target) return key;
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
