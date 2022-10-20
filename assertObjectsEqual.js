c8eddf170259bf341d80ea05f68cdf91ba33dd4b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 20:50:26 -0700

    Adds util library's inspect method

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -37,7 +37,8 @@ const eqObjects = function (object1, object2) {
 }
 
 const assertObjectsEqual = function(actual, expected) {
-  eqObjects(actual, expected) ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
+  const inspect = require('util').inspect;
+  eqObjects(actual, expected) ? console.log(`🌲🌲🌲 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`) : console.log(`🍄🍄🍄 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 }
 
 // Tests
