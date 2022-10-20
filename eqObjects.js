f05c9459c411f161c44d35714dacf940d7cf4663
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 20:19:30 -0700

    Adds eqArrays to be able to compare arrays

--- eqObjects.js
+++ eqObjects.js
@@ -12,21 +12,26 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqObjects = function (object1, object2) {
+  if(Object.keys(object1).length !== Object.keys(object2).length) return false;
   for (let key in object1) {
-    console.log(key);
-    console.log(object2[key]);
-    console.log(object1[key]);
-    if (object2[key] && object1[key] === object2[key]) {
-      return true
-    }
-    return false;
+    if (!object2[key] || object1[key] !== object2[key]) return false;
   }
+  return true;
 }
 
 // Tests
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc); // => false
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
