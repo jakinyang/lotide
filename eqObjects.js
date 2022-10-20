b25c9e5223ab4423a103057e877d727eb9ebec72
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-10-19 19:44:38 -0700

    Adds function body to eqObjects
    Function body with one for ... in loop that checks for extance of key in object1 in object2, then checks if the values stored in those keys is equal.
    
    If both pass, then that iteration returns true.

--- eqObjects.js
+++ eqObjects.js
@@ -3,9 +3,30 @@ Pseudocode
 
 1. Comparing objects will require iterating over keys, then comparing both keys, and the values therein.
 
+2. In iterating over the values, if the value doesn't "catch" and return true, then it will have to default to return false
 
 */
 
 const assertEqual = (actual, expected) => {
   actual === expected ? console.log(`🌲🌲🌲 Assertion Passed: ${actual} === ${expected}`) : console.log(`🍄🍄🍄 Assertion Failed: ${actual} !== ${expected}`);
 };
+
+const eqObjects = function (object1, object2) {
+  for (let key in object1) {
+    console.log(key);
+    console.log(object2[key]);
+    console.log(object1[key]);
+    if (object2[key] && object1[key] === object2[key]) {
+      return true
+    }
+    return false;
+  }
+}
+
+// Tests
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc); // => false
